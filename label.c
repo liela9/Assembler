ecd113765004101a8124bf031797e22070831b76
liela9 <72128162+liela9@users.noreply.github.com>
2022-07-21 18:35:21 +0300

    apdate "label"

--- label.c
+++ label.c
@@ -7,21 +7,20 @@
 
 
 /*Inserts new label to the label list*/
-int insert_new_label(char *name, int type, int address){
-    ptr_label lab;
+int insert_new_label(char *name, int type, int address, ptr_label head_label, ptr_label tail_label){
+    ptr_label temp_label;
 
-    lab = (ptr_label) malloc(sizeof(label));
-    if(!lab){
+    temp_label = (ptr_label) malloc(sizeof(label));
+    if(!temp_label){
         printf("Memory allocation failed\n");
         exit(0);
     }
 
-    
     name[sizeof(name)-1] = '\0'; /*Remove the char ':' */
 
-    if(label_exists(name)){
+    if(label_exists(name, &head_label)){
         printf("Error: %s label already exists!\n", name);
-        there_is_error = 1;
+        
     }
     
     if(saved_words(name)){
@@ -30,34 +29,34 @@ int insert_new_label(char *name, int type, int address){
     }
 
     else{
-        strcpy(lab->name, name);
-        strcpy(lab->type, type);
-        lab->dec_address = address;
-        lab->next = NULL;
+        strcpy(temp_label->name, name);
+        strcpy(temp_label->type, type);
+        temp_label->dec_address = address;
+        temp_label->next = NULL;
         
-        tail_label->next = lab;
-        tail_label = lab;
+        tail_label->next = temp_label;
+        tail_label = temp_label;
     }
-    
     
     return address;   
 }
 
 
 /*Checks if this label exists in this file*/
-int label_exists(char *name){
-    ptr_label local_label;
+int label_exists(char *name, ptr_label head_label){
+    ptr_label temp_label;
 
-    local_label = head_label;
+    temp_label = head_label;
 
-    while(local_label){
-        if(strcmp(local_label->name, name))
+    while(temp_label){
+        if(strcmp(temp_label->name, name))
             return 1;
-        local_label = local_label->next;
+        temp_label = temp_label->next;
     }
     return 0;
 }
 
+/*Checks if it is a saved word of the system*/
 int saved_words(char *name){ 
     int i;
     
@@ -79,37 +78,3 @@ int saved_words(char *name){
     return 0;
 }
 
-
-/*Fills the values for specific row*/
-int *fill_values(FILE *f, long int pos){
-    char token[];
-    int c, *val;
-
-    fseek(f, pos, SEEK_SET);
-    strtok(fgets(token, ROW_LENGTH, f), ' \t');
-    
-    
-    while(token != "\n"){
-        L++;
-        if((ZERO_ASCII_CODE <= token && token <= NINE_ASCII_CODE) || token[0] == '-' || token[0] == '+'){ /*If thats a number*/
-            *val = decimal_to_binary(token);
-            *val++;
-        }
-        else{ /*If thats a string*/
-            /*Remove the "" of the string*/
-            token[strlen(token)-1] = '\0';
-            *token++;
-
-            /*Read char by char*/
-            while(*token != '\0'){
-                c = *token;
-                *val = decimal_to_binary(c); 
-                *val++;
-                *token++;
-            }
-
-        }  
-        token = strtok(NULL, ', \t');
-    }
-    return 
-}
