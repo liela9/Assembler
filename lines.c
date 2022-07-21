394a99084ad43bacecbbf35fccdfc2e165a8dc95
liela9 <72128162+liela9@users.noreply.github.com>
2022-07-21 14:00:46 +0300

    "lines" file changes

--- lines.c
+++ lines.c
@@ -2,166 +2,205 @@
 #include "label.c"
 
 
-ptr_bin *binar_lines;
-char **label_apearances;
 
-
-void insert_new_order(char * label, int line, unsigned long op_code, char *source_op, char *dest_op){
+void insert_order(char *label, int IC, unsigned long op_code, char *source_op, char *dest_op){
     int source, dest;
+    char **struct_access1, **struct_access2;
+    unsigned int *orders_table;
+    
+    orders_table = (unsigned int*)malloc(5 * sizeof(unsigned int));
+
     source = 0;
     dest = 0;
-    char **struct_access1, **struct_access2;
 
 
     /*find the addressing type - שיטות מיעון*/
+    /*
+        Examples:
+
+        Label: X, STR
+        Struct: S.1
+        register: r1, r2
+        Value: 1 (for S.1), 2 (for S.2)  OR  #-5
+    */
     if(source = is_register(source_op) != -1){
         if(dest = is_register(dest_op) != -1){/*Two registers*/
-            create_new_line(label, line, op_code, convertDtoB(3), convertDtoB(3));
-            create_line_for_registers(source, dest, line++);
+            create_order_line(label, IC, op_code, convertDtoB(3), convertDtoB(3), &orders_table);
+            create_registers_line(source, dest, IC++, &orders_table);
         }
         else if(label_exists(dest_op)){/*Register and label*/
-                create_new_line(label, line, op_code, convertDtoB(3), convertDtoB(1));
-                create_line_for_registers(source, 0, line++);
-                create_unknown_line(line++);
+                create_order_line(label, IC, op_code, convertDtoB(3), convertDtoB(1), &orders_table);
+                create_registers_line(source, 0, IC++, &orders_table);
+                create_unknown_line(IC++, &orders_table);
             }
             else if(struct_access1 = is_struct(dest_op)){/*Register and struct*/
-                    create_new_line(label, line, op_code, convertDtoB(3), convertDtoB(2));
-                    create_line_for_registers(source, 0, line++);
-                    create_unknown_line(line++);
-                    create_line_for_value(struct_access1[1], line++);
+                    create_order_line(label, IC, op_code, convertDtoB(3), convertDtoB(2), &orders_table);
+                    create_registers_line(source, 0, IC++, &orders_table);
+                    create_unknown_line(IC++, &orders_table);
+                    create_index_line(struct_access1[1], IC++, &orders_table);
                 }
     }
         
     else if(label_exists(source_op)){
         if(dest = is_register(dest_op) != -1){/*Label and register*/
-            create_new_line(label, line, op_code, convertDtoB(1), convertDtoB(3));
-            create_unknown_line(line++);
-            create_line_for_registers(0, dest, line++);
+            create_order_line(label, IC, op_code, convertDtoB(1), convertDtoB(3), &orders_table);
+            create_unknown_line(IC++, &orders_table);
+            create_registers_line(0, dest, IC++, &orders_table);
         }
         else if(struct_access1 = is_struct(dest_op)){/*Label and struct*/
-                create_new_line(label, line, op_code, convertDtoB(1), convertDtoB(2));
-                create_unknown_line(line++);
-                create_unknown_line(line++);
-                create_line_for_value(struct_access1[1], line++);
+                create_order_line(label, IC, op_code, convertDtoB(1), convertDtoB(2), &orders_table);
+                create_unknown_line(IC++, &orders_table);
+                create_unknown_line(IC++, &orders_table);
+                create_index_line(struct_access1[1], IC++, &orders_table);
             }
             else if(label_exists(dest_op)){/*Label and label*/
-                    create_new_line(label, line, op_code, convertDtoB(1), convertDtoB(1));
-                    create_unknown_line(line++);
-                    create_unknown_line(line++);
+                    create_order_line(label, IC, op_code, convertDtoB(1), convertDtoB(1), &orders_table);
+                    create_unknown_line(IC++, &orders_table);
+                    create_unknown_line(IC++, &orders_table);
                 }
     }
         
     else if(source_op[0] == '#'){
         if(dest = is_register(dest_op) != -1){/*Value and register*/
-            create_new_line(label, line, op_code, convertDtoB(0), convertDtoB(3));
-            create_line_for_value(source_op, line++);
-            create_line_for_registers(0, dest, line++);
+            create_order_line(label, IC, op_code, convertDtoB(0), convertDtoB(3), &orders_table);
+            create_value_line(source_op, IC++, &orders_table);
+            create_registers_line(0, dest, IC++, &orders_table);
         }
         else if(label_exists(dest_op)){/*Value and label*/
-                create_new_line(label, line, op_code, convertDtoB(0), convertDtoB(1));
-                create_line_for_value(source_op, line++);
-                create_unknown_line(line++);
+                create_order_line(label, IC, op_code, convertDtoB(0), convertDtoB(1), &orders_table);
+                create_value_line(source_op, IC++, &orders_table);
+                create_unknown_line(IC++, &orders_table);
             }
             else if(struct_access1 = is_struct(dest_op)){/*Value and struct*/
-                    create_new_line(label, line, op_code, convertDtoB(0), convertDtoB(2));
-                    create_line_for_value(source_op, line++);
-                    create_unknown_line(line++);
-                    create_line_for_value(struct_access1[1], line++);
+                    create_order_line(label, IC, op_code, convertDtoB(0), convertDtoB(2), &orders_table);
+                    create_value_line(source_op, IC++, &orders_table);
+                    create_unknown_line(IC++, &orders_table);
+                    create_index_line(struct_access1[1], IC++, &orders_table);
                 }
     }
         
     
     else if(struct_access1 = is_struct(source_op)){
         if(dest = is_register(dest_op) != -1){/*Struct and register*/
-            create_new_line(label, line, op_code, convertDtoB(2), convertDtoB(3));
-            create_unknown_line(line++);
-            create_line_for_value(struct_access1[1], line++);
-            create_line_for_registers(0, dest, line++);
+            create_order_line(label, IC, op_code, convertDtoB(2), convertDtoB(3), &orders_table);
+            create_unknown_line(IC++, &orders_table);
+            create_index_line(struct_access1[1], IC++, &orders_table);
+            create_registers_line(0, dest, IC++, &orders_table);
         }
         else if(label_exists(dest_op)){/*Struct and label*/
-                create_new_line(label, line, op_code, convertDtoB(2), convertDtoB(1));
-                create_unknown_line(line++);
-                create_line_for_value(struct_access1[1], line++);
-                create_unknown_line(line++);
+                create_order_line(label, IC, op_code, convertDtoB(2), convertDtoB(1), &orders_table);
+                create_unknown_line(IC++, &orders_table);
+                create_index_line(struct_access1[1], IC++, &orders_table);
+                create_unknown_line(IC++, &orders_table);
             }
             else if(struct_access2 = is_struct(dest_op)){/*Struct and struct*/
-                    create_new_line(label, line, op_code, convertDtoB(2), convertDtoB(2));
-                    create_unknown_line(line++);
-                    create_line_for_value(struct_access1[1], line++);
-                    create_unknown_line(line++);
-                    create_line_for_value(struct_access2[1], line++);
+                    create_order_line(label, IC, op_code, convertDtoB(2), convertDtoB(2), &orders_table);
+                    create_unknown_line(IC++, &orders_table);
+                    create_index_line(struct_access1[1], IC++, &orders_table);
+                    create_unknown_line(IC++, &orders_table);
+                    create_value_line(struct_access2[1], IC++, &orders_table);
                 }
     }
 
 }
 
 
-/*Creates new machine code line (binary)*/
-void create_new_line(char *label_name, int line, unsigned long op_code, long source_op_addressing, long dest_op_addressing){
+/*Creates new machine code line in orderd_table*/
+int create_order_line(char *label_name, int IC, unsigned long op_code, long source_op_addressing, long dest_op_addressing, unsigned int *orders_table){
     
-    ptr_bin bin;
-
-    bin = (ptr_bin) malloc(sizeof(binaryMachineCode));
-    if(!bin){
-        printf("Memory allocation failed\n");
-        exit(0);
-    }
+    *(orders_table + IC) = ~0<<2; /*Two zeros in the right*/
+    *(orders_table + IC) = *(orders_table + IC)&(dest_op_addressing<<2);
+    *(orders_table + IC) = *(orders_table + IC)&(source_op_addressing<<4);
+    *(orders_table + IC) = *(orders_table + IC)&(op_code<<6);
     
-    bin->ARE = 0;
-    bin->destination = dest_op_addressing;
-    bin->source = source_op_addressing;
-    bin->opcode = op_code;
-
-    binar_lines[line] = &bin;
-
-    strcpy(label_apearances[line], label_name);
-
-    /*
-    binar_lines[line] = ~0<<2; 
-    binar_lines[line] = binar_lines[line]&(dest_op_addressing<<2);
-    binar_lines[line] = binar_lines[line]&(source_op_addressing<<4);
-    binar_lines[line] = binar_lines[line]&(op_code<<6);
-    */
+    return IC++;
 }
 
 
 /*Creates new machine code line (binary) for registers*/
-void create_line_for_registers(int source, int dest, int line){
+int create_registers_line(int source, int dest, int IC, unsigned int *orders_table){
     
-    binar_lines[line] = convertDtoB(source);//לבדוק שיש 4 ביטים
-    binar_lines[line] = convertDtoB(dest);//לבדוק שיש 4 ביטים
+    *(orders_table + IC) = *(orders_table + IC)&(convertDtoB(source)<<6);
+    *(orders_table + IC) = *(orders_table + IC)&(convertDtoB(dest)<<2);
+    
+    return IC++;
 }
 
 
 /*Gets numeric value as string*/
-void create_line_for_value(char *val, int line){
-    
-    binar_lines[line] = convertDtoB(atoi(val));//לבדוק שיש 10 ביטים
+int create_value_line(char *value, int IC, unsigned int *orders_table){
+    value[0] = '\0'; /*Removes the charactar # */
+    *(orders_table + IC) = convertDtoB(atoi(value));
+    return IC++;
 }
 
 
-/*At this point, the address is unknown*/
-void create_unknown_line(int line){
-    
-    binar_lines[line] = '?';
+int create_index_line(int number, int IC, unsigned int *orders_table){
+    *(orders_table + IC) = convertDtoB(number);
+    return IC++;
 }
 
 
-create_zero_line(int line){
-    ptr_bin bin;
+/*At this point, the address of the label(data) is unknown*/
+int create_unknown_line(int IC, unsigned int *orders_table){
+    
+    *(orders_table + IC) = '?';
+    
+    return IC++;
+}
 
-    bin = (ptr_bin) malloc(sizeof(binaryMachineCode));
-    if(!bin){
-        printf("Memory allocation failed\n");
-        exit(0);
+/*Creates new machine code line in data_table*/
+int create_data_line(int DC, char *row_content, unsigned int *data_table, char *type){
+    char *token, *string_part;
+    int number;
+
+    token = strtok(row_content, ' ,\t\r');
+
+    if(!strcmp(type, ".data")){
+        while (token){
+            number = atoi(token);
+            *(data_table + DC) = convertDtoB(number);
+            DC++;
+        }
     }
-    
-    bin->ARE = 0;
-    bin->destination = 0;
-    bin->source = 0;
-    bin->opcode = 0;
 
-    binar_lines[line] = &bin; 
+    if(!strcmp(type, ".string")){
+        int i;
+
+        for(i = 1; i < sizeof(token)-1; i++){/*Reads between the ""*/
+            *(data_table + DC) = convertDtoB(token[i]);/*Converts the ASCII code of the letter to binary*/
+            DC++;
+        }
+
+        create_zero_line(&data_table, DC);
+        DC++;
+    }
+
+    if(!strcmp(type, ".struct")){
+        int i;
+
+        number = atoi(token);
+        token = strtok(NULL, ' ,\t\r');
+
+        *(data_table + DC) = convertDtoB(number);
+        DC++;
+
+        for(i = 1; i < sizeof(token)-1; i++){/*Reads between the ""*/
+            *(data_table + DC) = convertDtoB(token[i]);/*Converts the ASCII code of the letter to binary*/
+            DC++;
+        }
+
+        create_zero_line(&data_table, DC);
+        DC++;
+    }
+
+    return DC;
+}
+
+
+/*Create zero line for '\0' at the end of string*/
+void create_zero_line(unsigned int *data_table, int DC){
+    *(data_table + DC) = convertDtoB(0);
 }
 
 
