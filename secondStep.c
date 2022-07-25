e49501e729538f5860e0daa70a1f405350d4cf1d
ToviKn <109872752+ToviKn@users.noreply.github.com>
2022-07-25 17:24:57 +0300

    Update secondStep.c

--- secondStep.c
+++ secondStep.c
@@ -1,14 +1,73 @@
 #include "assembler.h"
 
 
-int second_translation(FILE *f){
-    char *row_content;
+int second_translation(FILE *f)
+{
+	int i;
+	char *entryLable;
+	ptr_label_apearence temp;
+	ptr_label temp_label;
+
+   
+
+	/* pointer to labelApearance */
+	temp = (ptr_label_apearence) malloc(sizeof(label));
+
+	if(!temp)
+	{
+
+		printf("Memory allocation failed\n");
+
+		exit(0);
+
+	}
 
 
-    IC = 0;
-    //Reads a line
-    while(fgets(row_content, row_content_LENGTH, f)){
-        
-    }
+	/* pointer to label_table */
+
+        temp_label = (ptr_label) malloc(sizeof(label));
+
+        if(!temp_label)
+        {
+
+		printf("Memory allocation failed\n");
+
+		exit(0);
+
+        }
+
+	
+	/* take care in lebel as type entry */
+	for(i = 0; i < strlen(orders_table); i++)
+	{
+		if(orders_table[i] == '?')
+		{
+			/* find the name of label */
+			temp = labelApearance;
+			while (temp)
+			{
+				if((temp->index_in_orders_table) != i)
+					temp = temp->next;
+				else
+					entryLable = temp->name;
+			}
+			
+
+			/* find the eddress of the label */
+			temp_label = head_label;
+
+			while(temp_label)
+			{
+
+				if(strcmp(temp_label->name, entryLable))
+
+      					temp_label = temp_label->next;
+      				else
+      					orders_table[i] == temp_label->dec_address;
+      			}
+
+
+		}
+	}
 
 }
