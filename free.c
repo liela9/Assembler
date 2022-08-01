ac8af21f5bb2b7b6adf3392084f5ba726765fdc0
liela9 <72128162+liela9@users.noreply.github.com>
2022-08-01 12:18:36 +0300

    Changes before move  allocation structures in main

--- free.c
+++ free.c
@@ -2,80 +2,36 @@
 #include "free.h"
 
 
+
+#define FREE(type, head)    type temp;\
+                            while (head) {\
+                                temp = head;\
+                                head = head->next;\
+                                free(temp);\
+                            }
+                    
+
+
 /*Free all the lists in the project*/
 void free_lists(multiVars *vars){
-    free_label_list(&vars);
-    free_labelApearence_list(&vars);
-    free_data_table(&vars);
-    free_orders_table(&vars);
-}
 
-
-/*Free the labels list */
-void free_label_list(multiVars *vars){
-    ptr_label *temp;
-    
-    while (vars->head_label) {
-        temp = vars->head_label;
-        vars->head_label = vars->head_label->next;
-        free(temp);
-    }
-    vars->head_label = NULL;
-}
-   
-
-/*Free the label_apearences list */
-void free_labelApearence_list(multiVars *vars){
-    ptr_label_apearence *temp;
-    
-    while (vars->head_label) {
-        temp = vars->head_label;
-        vars->head_label = vars->head_label->next;
-        free(temp);
-    }
-    vars->head_label = NULL;
-}
-
-
-/*Free the data array*/
-void free_data_table(multiVars *vars){
+    FREE(ptr_label, vars->head_label)
+    FREE(ptr_label_apearence, vars->head_label_apear)
     free(vars->data_table);
-}
-
-
-/*Free the orders array*/
-void free_orders_table(multiVars *vars){
     free(vars->orders_table);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+   
+   
 /*
+void free_label_list(ptr_label head){
+    ptr_label temp;
 
-
-void free_list(char *type, void *head) {
-    void *temp;
-
-    
-    while (head != NULL) {
+    while (head) {
         temp = head;
         head = head->next;
-        
         free(temp);
     }
-    head = NULL;
-    
 }
+*/   
 
-
-*/
