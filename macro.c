f4448394cdf5cdb9435ac1497f6eb405ff0c823a
liela9 <72128162+liela9@users.noreply.github.com>
2022-08-02 13:41:05 +0300

    macros changes

--- macro.c
+++ macro.c
@@ -5,32 +5,39 @@
 
 
 /*Insert new macro to the list*/
-bool insert_macro(ptr_macro head_macro, ptr_macro *macro_pointer, char *macro_id){
-    ptr_macro temp;
-
-    temp = NULL;
-    /*TODO: check valid name for macro*/
-    if((*macro_pointer = get_macro_by_id(head_macro, macro_id))){
-        fprintf(stderr, "Can not add new macro %s, Macro already exists\n", macro_id);
-        return false;
-    }
-
-    (*macro_pointer)->macro_id = (char *)malloc(sizeof(char) * MAX_LINE_LENGTH);
-    strcpy((*macro_pointer)->macro_id, macro_id);
-    (*macro_pointer)->macro_content = (char *)malloc(sizeof(char) * MAX_LINE_LENGTH * MACRO_MAX_LINE_NUMBER);
-    (*macro_pointer)->macro_content[0] = '\0';
-    (*macro_pointer)->next = NULL;
-
-    if(!head_macro)
-        head_macro = *macro_pointer;
+ptr_macro create_macro_node(char *macro_id){
+    ptr_macro new_node;
+    new_node = NULL;
     
-    else {/*Insert the new node to the end of the list*/
-        temp = head_macro;
-        while (temp->next)
-            temp = temp->next;
-        temp->next = *macro_pointer;
+    new_node = (ptr_macro) malloc(sizeof(macro));
+    if (!new_node)
+    { 
+        /* TODO: memory allocation error message in MACRO */
+        printf("System Error: Memory allocation failed\n");
+        return NULL;
     }
-    return true;
+
+
+    new_node->macro_id = (char *)malloc(sizeof(macro_id));
+    if (!new_node->macro_id)
+    { 
+        printf("System Error: Memory allocation failed\n");
+        free(new_node);
+        return NULL;
+    }
+
+    new_node->macro_content = (char *)malloc(sizeof(char) * MAX_LINE_LENGTH * MACRO_MAX_LINE_NUMBER);
+    if (!new_node->macro_content)
+    { 
+        printf("System Error: Memory allocation failed\n");
+        free(new_node->macro_id);
+        free(new_node);
+        return NULL;
+    }
+    strcpy(new_node->macro_id, macro_id);
+    new_node->macro_content[0] = '\0';
+    new_node->next = NULL;
+    return new_node;
 }
 
 /*Finds macro by it's id and return it's pointer*/
@@ -47,6 +54,10 @@ ptr_macro get_macro_by_id(ptr_macro head_macro, char *macro_id){
     return NULL;
 }
 
+
+bool macro_exists(ptr_macro head_macro, char *macro_id) {
+    return (get_macro_by_id(head_macro, macro_id) != NULL);
+}
 
 /*Prints the macro list */
 void print_macro_list(ptr_macro head_macro) {
